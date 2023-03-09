fcf471a6f0096d925252b27baab31caa0e9defe3
shreyas-acharya <acharyashreyas14@gmail.com>
2023-03-09 14:50:27 +0530

    Update output.yml and test.py file: Store the output value using console.print inside test.py file

--- test.py
+++ test.py
@@ -12,4 +12,4 @@ table.add_row("2", "job2", "done")
 table.add_row("3", "job3", "not completed")
 table.add_row("4", "job4", "done")
 
-console.print(table)
+print(f'"value={console.print(table)}" >> $GITHUB_OUTPUT')
